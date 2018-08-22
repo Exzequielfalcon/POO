a392f61618f710a9f73e4b6e0559ad26f6a070ee
exze <exzequielfalcon@gmail.com>
2018-08-22 13:42:38 -0300

    POOAgenda Reunion

--- POOAgenda/src/Reunion.java
+++ POOAgenda/src/Reunion.java
@@ -1,18 +1,18 @@
-import java.lang.Object;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Reunion {
-	private final int N=100;
-	private int CantAsis=0;
+	private final int N = 100;
+	private int CantAsis = 0;
 	private String lugar;
 	private String tema;
 	private LocalDate fecha;
 	private LocalTime hora;
 	private int duracion;
 	private Contacto[] asistentes;
-	
-	public Reunion(String lugar, String tema, LocalDate fecha, LocalTime hora,
-			int duracion) {
-		this.CantAsis=0;
+
+	public Reunion(String lugar, String tema, LocalDate fecha, LocalTime hora, int duracion) {
+		this.CantAsis = 0;
 		this.lugar = lugar;
 		this.tema = tema;
 		this.fecha = fecha;
@@ -20,30 +20,26 @@ public class Reunion {
 		this.duracion = duracion;
 		this.asistentes = new Contacto[N];
 	}
-	
-	public void AgregarAsistente(Contacto c){
-		if (!this.asiste(c)){
-			asistentes[CantAsis]=c;
+
+	public void AgregarAsistente(Contacto c) {
+		if (!this.asiste(c)) {
+			asistentes[CantAsis] = c;
 			CantAsis++;
 		}
 	}
-	
-	public void reporogramar(LocalDate fn, LocalTime hn){
-		this.fecha=fn;
-		this.hora=hn;
+
+	public void reporogramar(LocalDate fn, LocalTime hn) {
+		this.fecha = fn;
+		this.hora = hn;
 	}
-	
-	public boolean asiste (Contacto c){
-		for (int i=0;i<CantAsis;i++){
-			if (c.equals(asistentes[i])){
+
+	public boolean asiste(Contacto c) {
+		for (int i = 0; i < CantAsis; i++) {
+			if (c.equals(asistentes[i])) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	
-	
-	
-}
 
+}
