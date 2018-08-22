bb69b90e0c79d89df47b5e29c9dde371fd697f78
exze <exzequielfalcon@gmail.com>
2018-08-22 20:46:15 -0300

    Main

--- POOAgenda/src/Agenda.java
+++ POOAgenda/src/Agenda.java
@@ -1,4 +1,37 @@
 
 public class Agenda {
+	private Contacto dueño;
+	private Contacto contactos[];
+	private Reunion reuniones[];
+
+	public Agenda(Contacto dueño, Contacto[] contactos, Reunion[] reuniones) {
+		this.dueño = dueño;
+		this.contactos = contactos;
+		this.reuniones = reuniones;
+	}
+
+	public Contacto getDueño() {
+		return dueño;
+	}
+
+	public void setDueño(Contacto dueño) {
+		this.dueño = dueño;
+	}
+
+	public Contacto[] getContactos() {
+		return contactos;
+	}
+
+	public void setContactos(Contacto[] contactos) {
+		this.contactos = contactos;
+	}
+
+	public Reunion[] getReuniones() {
+		return reuniones;
+	}
+
+	public void setReuniones(Reunion[] reuniones) {
+		this.reuniones = reuniones;
+	}
 
 }
